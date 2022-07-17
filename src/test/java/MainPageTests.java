33f87ea1bdfc103c7e4f9a4d123206b4ee78a78c
Natalya.Kadysheva <Native1331>
2022-07-18 02:07:14 +0300

    Add Tests

--- src/test/java/MainPageTests.java
+++ src/test/java/MainPageTests.java
@@ -1,4 +1,5 @@
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
@@ -6,6 +7,7 @@ import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.*;
 import static io.qameta.allure.Allure.step;
 
+@Tag("TutuTests")
 public class MainPageTests extends TestBase {
     @Test
     @DisplayName("Find Avia tickets")
