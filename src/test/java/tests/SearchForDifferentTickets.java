861109420e45497665e009a2bb216fb62800db1a
Natalya Kadysheva <83497921+Native1331@users.noreply.github.com>
2022-07-22 03:12:18 +0300

    Update SearchForDifferentTickets.java

--- src/test/java/tests/SearchForDifferentTickets.java
+++ src/test/java/tests/SearchForDifferentTickets.java
@@ -11,7 +11,7 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
 import static io.qameta.allure.Allure.step;
 
-@Tag("tutuRuTests")
+@Tag("TutuRuTests")
 public class SearchForDifferentTickets extends TestBase {
 
     @ValueSource(strings = {
